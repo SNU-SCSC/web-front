816df9734fd4ebc6d63d54cd61bb152d3e57cfe7
polyxscl <polyxscl@gmail.com>
2024-02-27 16:26:41 +0900

    ClickButton update

--- src/components/interface/ClickButton.tsx
+++ src/components/interface/ClickButton.tsx
@@ -1,26 +1,55 @@
+"use client";
 import MultiClassName from "@/util/MultiClassName";
 import "./ClickButton.css";
+import MultiStyles from "@/util/MultiStyles";
+import { useRef } from "react";
 
-export type ClickButtonTypes = "accent" | "positive" | "negative" | "alternate";
+export const ClickButtonColors = {
+    "normal": "var(--normal)",
+    "accent": "var(--accent)",
+    "positive": "var(--positive)",
+    "negative": "var(--negative)",
+    "alternate": "var(--alternate)"
+}
+
+export const ClickButtonColorSubs = {
+    "normal": "var(--normal-sub)",
+    "accent": "var(--accent-sub)",
+    "positive": "var(--positive-sub)",
+    "negative": "var(--negative-sub)",
+    "alternate": "var(--alternate-sub)"
+}
+
+export type ClickButtonColorTypes = keyof typeof ClickButtonColors;
+
+export type ClickButtonProps = {
+    colorType?: ClickButtonColorTypes,
+} & React.ButtonHTMLAttributes<HTMLButtonElement> & Partial<Record<ClickButtonColorTypes, boolean>>;
 
 export default function ClickButton({
     children = "Button",
     className,
-    accent,
-    positive,
-    negative,
-    alternate,
+    style,
+    colorType = "accent",
+    onClick,
     ...props
-}: React.ButtonHTMLAttributes<HTMLButtonElement> & Partial<Record<ClickButtonTypes, boolean>>) {
+}: ClickButtonProps) {
+    let ref = useRef<HTMLButtonElement>(null);
+
     return (
         <button className={MultiClassName([
             "click-button",
-            accent && "accent",
-            positive && "positive",
-            negative && "negative",
-            alternate && "alternate",
             className,
-        ])} {...props}>
+        ])} style={MultiStyles([
+            style,
+            {
+                "--click-button-color": ClickButtonColors[colorType],
+                "--click-button-color-sub": ClickButtonColorSubs[colorType]
+            } as React.CSSProperties
+        ])} onClick={ (...args) => {
+            ref.current?.blur();
+            onClick && onClick(...args);
+        }} ref={ref} {...props}>
             {children}
         </button>
     );
